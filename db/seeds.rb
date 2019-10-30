91303ccf4934a609516600a04cf7f2600865845f
Stephanie Delbecque <stephanie.delbecque@gmail.com>
2019-10-30 15:46:34 +0100

    completed Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+resto1 = Restaurant.create(name:"Italia", address: "Rome", category: "italian")
+resto2 = Restaurant.create(name:"Jappy", address: "tokyo", category: "japanese")
+resto3 = Restaurant.create(name:"France", address: "Paris", category: "french")
+resto4 = Restaurant.create(name:"Canton", address: "Shangai", category: "chinese")
+resto5 = Restaurant.create(name:"le choux", address: "Brussels", category: "belgian")
